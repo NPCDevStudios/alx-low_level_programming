6f6e26825c68e9a7db2fdaf63ca52c7ded978c93
ALXwsl <Lokmen.556@live.com>
2023-05-23 18:31:37 +0100

    Fix Task 0.0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,7 +11,7 @@ int main(void)
 	char str[] = "_putchar";
 	int i, strlen = (sizeof(str) / sizeof(str[0]));
 
-	for (i = 0; i <= strlen; i++)
+	for (i = 0; i < strlen-1; i++)
 	{
 		_putchar(str[i]);
 	}
