a3f652475bacc5bf0e2af09e6755f6548fe73d7a
ALXwsl <Lokmen.556@live.com>
2023-05-23 18:53:01 +0100

    Fix Last digit.1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * print_last_digit - Returns last digit of given integer
@@ -11,9 +12,15 @@ int print_last_digit(int n)
 {
 	int lastDig = n % 10;
 
-	if (n < 0)
-		lastDig = ( (n * (-1)) % 10);
+	if (n == INT_MIN)
+	{
+		_putchar('8');
+		return 8;
+	}
 
-	_putchar(lastDig % 10 + '0');
+	if (n < 0)
+		lastDig = ( (n * -1) % 10);
+
+	_putchar(lastDig + '0');
 	return (lastDig);
 }
