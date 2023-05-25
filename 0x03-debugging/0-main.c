8f51bc2740f92b0f41e130ffd8d33a0ccf5d32a1
ALXwsl <Lokmen.556@live.com>
2023-05-25 10:15:29 +0100

    Debug 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Print whether random generated value is positive or negative
+ *
+ * Return: 0 when done, 1 if erro
+ * --- by NPCdev ---
+*/
+void positive_or_negative(int n);
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+		if (n > 0)
+		printf("%d is positive\n", n);
+		else
+		if (n < 0)
+		printf("%d is negative\n", n);
+		else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
