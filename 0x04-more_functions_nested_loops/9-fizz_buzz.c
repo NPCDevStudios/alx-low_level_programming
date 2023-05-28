e99e4b5f733d6d0ca4f948326d64330dbe01e78a
ALXwsl <Lokmen.556@live.com>
2023-05-28 13:06:35 +0100

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print Fizz-Buzz
+ * Return: 0 when done, 1 if fail
+ * --- by NPCdev ---
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", i);
+		if (i != 100)
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
