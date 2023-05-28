b0a4299dab4f58e73f57898285dae87dd8004a8a
ALXwsl <Lokmen.556@live.com>
2023-05-28 13:25:46 +0100

    Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle based on given integer
+ * @size: given integer
+ * Return: void
+ * --- by NPCdev ---
+*/
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+			_putchar(' ');
+		for (j = 0; j <= i; j++)
+			_putchar('#');
+	_putchar('\n');
+	}
+}
