f42759ea4e40853b1bb11e1b1a50c6fd2f6c1fd9
ALXwsl <Lokmen.556@live.com>
2023-05-30 11:21:57 +0100

    Fix task 7.1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,8 +13,15 @@ void puts_half(char *str)
 
 	while (str[len] != '\0')
 		len++;
-
-	for (i = (len / 2) + 1; i < len; i++)
-		_putchar(str[i]);
+	if (len % 2 != 0)
+	{
+		for (i = (len / 2) + 1; i < len; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = len / 2; i < len; i++)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
